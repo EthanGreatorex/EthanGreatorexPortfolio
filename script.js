04c7761a595242e9a7d122a108b7a272bfa2e18a
EggGreatorex <eggGreatorex@gmail.com>
2024-11-28 21:50:33 +0000

    added files

---
+++ script.js
@@ -0,0 +1,8 @@
+
+let mybutton = document.getElementById("upButton");
+
+
+function scrollToTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
